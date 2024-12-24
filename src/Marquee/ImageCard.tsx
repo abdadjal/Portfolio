9784ddd4c6443e322f11f5064bed6037a498c060
Abderahman <abd.adj04@gmail.com>
2024-12-24 22:05:07 +0100

    trying to fix mobile anim pb

--- src/Marquee/ImageCard.tsx
+++ src/Marquee/ImageCard.tsx
@@ -8,13 +8,14 @@ function ImageCard(props: propsImage) {
         <div className="shrink-0 h-full py-1 transition-transform duration-300">
             <img 
                 src={props.img} 
-                className="aspect-video object-cover h-full w-full object-top rounded-lg ring-1 ring-white ring-opacity-10"
+                className="h-full w-auto max-h-[250px] md:max-h-full object-cover rounded-lg ring-1 ring-white ring-opacity-10"
                 alt="Design work"
                 loading="eager"
             />
         </div>
     );
 }
+
 
 
 export default ImageCard;
