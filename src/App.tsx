707b40d6fcf68ad9d3d17761e6264d8d3a20a3b5
Abderahman <abd.adj04@gmail.com>
2025-01-15 12:28:22 +0100

    changing things

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import Cards from './Cards/Cards.tsx';
 import Header from './Header.tsx';
-import AnimatedContainer from './AnimatedContainer.tsx';
 
 function App() {
 
   return (
-    <AnimatedContainer>
+    <>
       <Header />
       <Cards />
-    </AnimatedContainer>
+    </>
   )
 }
 
